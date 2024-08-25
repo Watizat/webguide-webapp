652333fbd05479943fb086acb4e3f866066037fe
Lucas F <lucas_fillon@proton.me>
2024-08-25 21:45:48 +0200

    Add intellisense completion for environnement variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+/** 
+ * Definition des variables d'environnement pour l'autocompletion Intellisense.
+ *
+ * Utiliser import.meta.env pour importer les variables.
+ * 
+ * Plus d'informations sur https://vitejs.dev/guide/env-and-mode#intellisense-for-typescript
+*/
+interface ImportMetaEnv {
+  readonly VITE_WEB_GUIDE_URL: string;
+  readonly VITE_BACKEND_URL: string;
+}
